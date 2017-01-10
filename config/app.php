5b4b0f30995c67ab0e36d5767deb995f41c81293
Sendy Halim <sendyhalim93@gmail.com>
2017-01-10 11:17:56 +0700

    Add FrozenNode/Laravel-Administrator as dependency

--- config/app.php
+++ config/app.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -166,8 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        //
+        Frozennode\Administrator\AdministratorServiceProvider::class,
 
         /*
          * Application Service Providers...
